180018c46510ea62605180ce4a2d8dfdebe4446b
40sinfreda <h.israel2008@gmail.com>
2026-09-16 10:56:34 +0300

    Connect Apps Script /exec bridge

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.HAZMANOT_WEBAPP = "";
+window.HAZMANOT_WEBAPP = "https://script.google.com/macros/s/AKfycbzJ6pmFEiY80GyBFEOy7jYXLu8O5eZfT00mMTYmyRytxtHDdBKXXJZcP72u3cFnkhin/exec";
 window.HAZMANOT_TOKEN = "hazmanot-2026-hadar";
