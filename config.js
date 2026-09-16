24644e61c9980f62aa311a0aef3c825b468d1b8e
40sinfreda <h.israel2008@gmail.com>
2026-09-16 11:10:46 +0300

    Point config to working /exec with doGet

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.HAZMANOT_WEBAPP = "https://script.google.com/macros/s/AKfycbzJ6pmFEiY80GyBFEOy7jYXLu8O5eZfT00mMTYmyRytxtHDdBKXXJZcP72u3cFnkhin/exec";
+window.HAZMANOT_WEBAPP = "https://script.google.com/macros/s/AKfycbwWvbE0V02q25L7POFj2Aug3OQ_lRQ30ppKaU8AY59qS3gcZpVyC6ZUCyBLD6abqqcF/exec";
 window.HAZMANOT_TOKEN = "hazmanot-2026-hadar";
